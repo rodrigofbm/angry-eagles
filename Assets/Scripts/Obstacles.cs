e21f1cdab2e8013bf7c16653f61dc1bc33420178
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2020-03-30 17:14:30 -0300

    added destructible obstacles

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour {
+    int health = 0;
+    SpriteRenderer spriteRender;
+    [SerializeField]
+    Sprite[] sprites;
+    [SerializeField]
+    GameObject destroyFx;
+
+    void Start() {
+        spriteRender = GetComponent<SpriteRenderer>();
+        spriteRender.sprite = sprites[health];
+    }
+
+    
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.relativeVelocity.magnitude > 4.0f && other.relativeVelocity.magnitude < 10.0f) {
+            UpdateObstacleSprite();
+        } else if (other.relativeVelocity.magnitude >12.0f && other.gameObject.CompareTag("Player")) {
+            RenderDestroyFx();
+            Destroy(gameObject);
+        }
+    }
+
+    void UpdateObstacleSprite() {
+        if(health < sprites.Length - 1) {
+            health++;
+            spriteRender.sprite = sprites[health];
+        }else {
+            RenderDestroyFx();
+            Destroy(gameObject);
+        }
+    }
+
+    void RenderDestroyFx() {
+        Instantiate(destroyFx, transform.position, Quaternion.identity);
+    }
+}
--- Assets/Scripts/PlayerDeathFx.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class PlayerDeathFx : MonoBehaviour
-{
-    void Start() {
-        Destroy(gameObject, 3.0f);
-    }
-
-}
